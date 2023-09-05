e77f86f39ad94beae5e6b3f2448ec83168dae9c3
Livelyoneweek <wlvvwhs@naver.com>
2023-09-05 23:30:19 +0900

    feat: css 방법1

--- componenets/NavBar.js
+++ componenets/NavBar.js
@@ -1,16 +1,18 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
     const router = useRouter();
     return (
         <nav>
-            <Link href="/" className="link1" style={ {color:router.pathname ==="/"? "red": ""} }>
+            <Link href="/" className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`} >
                  Home
             </Link>
             
-            <Link href="/about">
-                About
+            <Link href="/about" className={[styles.link, 
+            router.pathname === "/about" ? styles.active : "",].join(" ")}>
+            About
             </Link>
         </nav>
     )
